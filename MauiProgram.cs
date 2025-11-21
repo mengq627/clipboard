29bf128b34838e8cb4e993f9a6e02fa1b67b7510
MengQ <1844325293@qq.com>
2025-11-21 19:09:39 +0800

    add singleInstance

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -2,14 +2,40 @@
 using clipboard.Services;
 #if WINDOWS
 using clipboard.Platforms.Windows.Services;
+using System.Threading;
 #endif
 
 namespace clipboard
 {
     public static class MauiProgram
     {
+        private static Mutex? _singleInstanceMutex;
+
         public static MauiApp CreateMauiApp()
         {
+#if WINDOWS
+            // 检查是否已有实例在运行
+            const string mutexName = "Global\\ClipboardManager_SingleInstance";
+            bool createdNew;
+
+            // 确保在应用退出时释放 Mutex
+            AppDomain.CurrentDomain.ProcessExit += (s, e) =>
+            {
+                _singleInstanceMutex?.ReleaseMutex();
+                _singleInstanceMutex?.Dispose();
+            };
+
+            _singleInstanceMutex = new Mutex(true, mutexName, out createdNew);
+            
+            if (!createdNew)
+            {
+                // 已有实例在运行，退出当前进程
+                System.Diagnostics.Debug.WriteLine("Another instance is already running. Exiting...");
+                Environment.Exit(0);
+                return null!; // 不会执行到这里，但为了编译通过
+            }
+#endif
+
             var builder = MauiApp.CreateBuilder();
             builder
                 .UseMauiApp<App>()
